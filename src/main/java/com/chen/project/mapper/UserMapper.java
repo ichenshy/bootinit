982685309489a89574b03210f90c784c55244151
ChenShengyuan <chen.shengyuan3@iwhalecloud.com>
2023-07-20 17:29:40 +0800

    csy

---
+++ src/main/java/com/chen/project/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.chen.project.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.chen.project.model.entity.User;
+
+/**
+ * @Entity com.yupi.project.model.domain.User
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
